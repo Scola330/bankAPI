5d6980b806ca6398df2173b95604257c26d1931c
Scola330 <155664435+Scola330@users.noreply.github.com>
2024-10-11 09:46:59 +0200

    Update Account.php

--- Account.php
+++ Account.php
@@ -20,7 +20,7 @@ class Account
     {
         $result = $db->query("SELECT user.id_account ,  konto.money_value , konto.money_type, user.email FROM user , konto WHERE user.id_account = $accountNo AND konto.ID = user.id_account");
         $account = $result->fetch_assoc();
-        $account = new Account($account['id_account'], $account['money_value'] . $account['money_type'], $account['email']);
+        $account = new Account($account['id_account'], $account['money_value'] , $account['money_type'], $account['email']);
         return $account;
     }
     public function getArray() : array
