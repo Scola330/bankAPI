25910b10fcddf61218fccc1151822171fd4580de
Michał Kowalczyk <michal.kowalczyk5@teb.edu.pl>
2024-10-11 09:30:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+require_once('Route.php');
+require_once('Account.php');
+use Steampixel\Route;
+use BankAPI\Account;
+require_once('Account.php');
+$db = new mysqli('localhost', 'root', '', 'banco_de_skibidi');
+$db->set_charset('utf8');
+if ($db->connect_errno) {
+    die('Failed to connect to MySQL: ' . $db->connect_error);
+}
+Route::add('/', function() {
+    echo 'Hello world!';
+  });
+
+Route::add('/account/([0-9]*)', function($accountNo) use($db){
+    $account = Account::getAccountNo($accountNo, $db);
+    header(('Content-Type: application/json'));
+    return json_encode($account->getArray());   
+});
+
+Route::run('/bankAPI');
+
+$db->close();
+?>
